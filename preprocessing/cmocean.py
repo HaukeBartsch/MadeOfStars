d8d75e6c860bc8120436df172ff9c5aaa0b71606
Hauke Bartsch <HaukeBartsch@gmail.com>
2025-08-16 12:04:07 +0200

    comment on the colormap format

--- preprocessing/cmocean.py
+++ preprocessing/cmocean.py
@@ -15,6 +15,7 @@ for colname in cmaps.keys():
        continue
     cm = cmocean.tools.get_dict(cmaps[colname], N=3)
     
+    # Question, why are there 3 entries for red and one color in the colormap? Use the last of those 3.
     col1 = '#%02x%02x%02x' % (int(cm['red'][0][2] * 255), int(cm['green'][0][2] * 255), int(cm['blue'][0][2] * 255))
     col2 = '#%02x%02x%02x' % (int(cm['red'][1][2] * 255), int(cm['green'][1][2] * 255), int(cm['blue'][1][2] * 255))
     col3 = '#%02x%02x%02x' % (int(cm['red'][2][2] * 255), int(cm['green'][2][2] * 255), int(cm['blue'][2][2] * 255))
