175c35ea83e9880219191c339c14ec6aacdec153
LennartElbe <lenni.elbe@gmail.com>
2025-08-18 13:58:48 -0700

    added toggleable pos/vel/neighbors options for sound and tweaked how sounds sound

--- js/World/AudioPlayer.js
+++ js/World/AudioPlayer.js
@@ -1,5 +1,9 @@
-const pentatonicIntervals = [0, 2, 4, 7, 9]; // semitones in pentatonic scale
-const rootFreq = 196;
+const intervals = [0, 2, 4, 7, 9]; // semitones in pentatonic scale
+// alternative intervals
+//const intervals = [0, 4, 7]; // semitones in major arpeggio
+//const intervals = [0]; // one note
+//const intervals = [0, 5, 7]; // semitones in major arpeggio
+const rootFreq = 30;
 const DURATION_FACTORS = {
   position:   1.0,
   velocity:   0.5,
@@ -43,18 +47,28 @@ class AudioPlayer {
 
     playNote() {
         var spectrums = []; // on per channel enabled
+        let numBands = 256;
         if (this.agents.enableChannel1)
-            spectrums.push(this.generateSpectrum(this.agents, 0, 64, 'position'));
-            spectrums.push(this.generateSpectrum(this.agents, 0, 64, 'velocity'));
-            spectrums.push(this.generateSpectrum(this.agents, 0, 64, 'neighborhood'));
+            if (this.usePos)
+                spectrums.push(this.generateSpectrum(this.agents, 0, numBands, 'position'));
+            if (this.useVel)
+                spectrums.push(this.generateSpectrum(this.agents, 0, numBands, 'velocity'));
+            if (this.useNb)
+                spectrums.push(this.generateSpectrum(this.agents, 0, numBands, 'neighborhood'));
         if (this.agents.enableChannel2)
-            spectrums.push(this.generateSpectrum(this.agents, 1, 64, 'position'));
-            spectrums.push(this.generateSpectrum(this.agents, 1, 64, 'velocity'));
-            spectrums.push(this.generateSpectrum(this.agents, 1, 64, 'neighborhood'));
+            if (this.usePos)
+                //spectrums.push(this.generateSpectrum(this.agents, 1, numBands, 'position'));
+            if (this.useVel)
+                //spectrums.push(this.generateSpectrum(this.agents, 1, numBands, 'velocity'));
+            if (this.useNb)
+                //spectrums.push(this.generateSpectrum(this.agents, 1, numBands, 'neighborhood'));
         if (this.agents.enableChannel3)
-            spectrums.push(this.generateSpectrum(this.agents, 2, 64, 'position'));
-            spectrums.push(this.generateSpectrum(this.agents, 2, 64, 'velocity'));
-            spectrums.push(this.generateSpectrum(this.agents, 2, 64, 'neighborhood'));
+            if (this.usePos)
+                //spectrums.push(this.generateSpectrum(this.agents, 2, numBands, 'position'));
+            if (this.useVel)    
+                //spectrums.push(this.generateSpectrum(this.agents, 2, numBands, 'velocity'));
+            if (this.useNb)
+                //spectrums.push(this.generateSpectrum(this.agents, 2, numBands, 'neighborhood'));
         this.resumeAudioContext();
         // offset the three sounds
         for (var i = 0; i < spectrums.length; i++)
@@ -68,50 +82,93 @@ class AudioPlayer {
     }
 
     getPentatonicFreq(i) {
-        // cycle through pentatonic intervals every 5 notes
-        const MAX_OCTAVE = 2;          // ← change as needed
+        const MAX_OCTAVE = 3;
         const octave  = Math.min(
-            Math.floor(i / pentatonicIntervals.length),
+            Math.floor(i / intervals.length),
             MAX_OCTAVE
         );
-        let scaleDegree = pentatonicIntervals[i % pentatonicIntervals.length];
+        let scaleDegree = intervals[i % intervals.length];
         let semitones = octave * 12 + scaleDegree;
 
         return rootFreq * Math.pow(2, semitones / 12);
     }
 
-    playSpectrum(spectrumAndNote, bpm, delay) {
+    playSpectrum(noteData, bpm, delay) {
         let now = this.audioCtx.currentTime;
-        let spectrum = spectrumAndNote[0];
-        let noteDurationFactor = spectrumAndNote[1];
+        let spectrum = noteData[0];
+        let noteDurationFactor = noteData[1];
+        let noteType = noteData[2];
         spectrum.forEach((amplitude, i) => {
-            let adjustedNow = now + i * 0.01 + delay;
+            let adjustedNow = now + i * 0.0001;
             if (amplitude < 0.01) return;
-            let filter = this.audioCtx.createBiquadFilter();
-            filter.type = 'lowpass';
-            filter.frequency.setValueAtTime(1000, adjustedNow);
 
             let osc = this.audioCtx.createOscillator();
-            let gain = this.audioCtx.createGain();
-
+            
             let freq = this.getPentatonicFreq(i);
             osc.frequency.value = freq;
-            osc.type = 'sawtooth'
-
+            osc.type = 'triangle'
+            
             let noteDuration = (60 / bpm)*noteDurationFactor; // would need to get shorter if we play more notes
-            let releaseTime = 0.05;
+            let gain = this.audioCtx.createGain();
+            
+            if (noteType == 'wobble') {
+                let attackTime = 0.00;
+                let baseAmp = amplitude * 0.05;
+                if (noteDurationFactor <= 0.1)
+                    baseAmp = amplitude * 0.8
+                let peakAmp = amplitude * 0.15;
+                gain.gain.setValueAtTime(0.0, adjustedNow)
+                //gain.gain.setValueAtTime(baseAmp, adjustedNow + attackTime);
+                gain.gain.linearRampToValueAtTime(peakAmp, adjustedNow + noteDuration / 2);
+                gain.gain.linearRampToValueAtTime(0, adjustedNow + noteDuration);
+                osc.connect(gain);
+                gain.connect(this.audioCtx.destination);
+            } else if (noteType == 'percuss') {
+                // delay effect
+                let dryGain = this.audioCtx.createGain();
+                let wetGain = this.audioCtx.createGain();
+                dryGain.gain.value = 1.0;          // original note volume
+                wetGain.gain.value = 0.3;          // echo volume
 
-            let baseAmp = amplitude * 0.05;
-            if (noteDurationFactor <= 0.1)
-                baseAmp = amplitude * 0.8
-            let peakAmp = amplitude * 0.15;
+                let delay = this.audioCtx.createDelay();
+                delay.delayTime.value = 0.3;
+                
+                let feedback = this.audioCtx.createGain();
+                feedback.gain.value = 0.4;
 
-            gain.gain.setValueAtTime(baseAmp, adjustedNow);
-            gain.gain.linearRampToValueAtTime(peakAmp, adjustedNow + noteDuration / 2);
-            gain.gain.linearRampToValueAtTime(baseAmp, adjustedNow + noteDuration - releaseTime);
-            gain.gain.linearRampToValueAtTime(0, adjustedNow + noteDuration);
+                delay.connect(feedback);
+                feedback.connect(delay);
 
-            osc.connect(gain).connect(filter).connect(this.audioCtx.destination);
+                osc.connect(gain);
+
+                let baseAmp = amplitude;
+                let endAmp = 0.0;
+                let attackTime = 0.03
+                gain.gain.setValueAtTime(0.0, adjustedNow);
+                gain.gain.setValueAtTime(baseAmp, adjustedNow+attackTime);
+                gain.gain.linearRampToValueAtTime(endAmp, adjustedNow + noteDuration - 0.01);
+                
+                gain.connect(dryGain);
+                gain.connect(delay);
+
+                delay.connect(wetGain);
+
+                dryGain.connect(this.audioCtx.destination);
+                wetGain.connect(this.audioCtx.destination);
+
+            } else if (noteType == 'flat') {
+                let attackTime = 0.05;
+                let baseAmp = amplitude;
+                if (noteDurationFactor <= 0.1)
+                    baseAmp = amplitude * 0.8
+                let peakAmp = amplitude * 0.15;
+                gain.gain.setValueAtTime(0.0, adjustedNow)
+                gain.gain.setValueAtTime(baseAmp, adjustedNow + attackTime);
+                gain.gain.linearRampToValueAtTime(peakAmp, adjustedNow + noteDuration / 2);
+                gain.gain.linearRampToValueAtTime(0, adjustedNow + noteDuration);
+                osc.connect(gain);
+                gain.connect(this.audioCtx.destination);
+            };
             osc.start(adjustedNow);
             osc.stop(adjustedNow + noteDuration);
         });
@@ -160,15 +217,16 @@ class AudioPlayer {
             // one component based on distance and speed
             // one component based on average clustering (per channel)
             // one component based on synchronized firing
-            let energy01 = (1/posMag);
+            let energy01 = (posMag);
             let energy02 = (velMag);
             //let energy03 = (cs);
             let energy04 = (ns);
+            let energy04_normalized = Math.min(energy04 / this.agents.count, 1);
 
-            let band01 = Math.floor((energy01 % 1) * numBands);
-            let band02 = Math.floor((energy02 % 1) * numBands);
+            let band01 = numBands - 1 - Math.floor((energy01 % 1) * numBands);
+            let band02 = numBands - 1 - Math.floor((energy02 % 1) * numBands);
             //let band03 = Math.floor((energy03 % 1) * numBands);
-            let band04 = Math.floor((energy04 % 1) * numBands);
+            let band04 = numBands - 1 - Math.floor((energy04 % 1) * numBands);
 
             if (energy_source == 'position')
                 spectrum[band01] += energy01;
@@ -179,11 +237,22 @@ class AudioPlayer {
                 spectrum[band04] += energy04;
         }
 
+        const noteDurationFactor = DURATION_FACTORS[energy_source];
         let max = Math.max(...spectrum);
         if (max === 0) return spectrum;
-        
-        const noteDurationFactor = DURATION_FACTORS[energy_source];
-        return [spectrum.map(val => val / max), noteDurationFactor];
+        let noteType = ''
+        if (noteDurationFactor == 1.0) {
+            noteType = 'wobble';
+        } else if (noteDurationFactor == 0.5) {
+            noteType = 'flat';
+        } else if (noteDurationFactor == 0.1)
+            noteType = 'percuss'
+        return [spectrum.map(val => val / max), noteDurationFactor, noteType];
+    }
+    setEnergyID(on=[TRUE,TRUE,TRUE]) {
+        this.usePos = on[0];
+        this.useVel = on[1];
+        this.useNb = on[2];
     }
 }
 export { AudioPlayer };
